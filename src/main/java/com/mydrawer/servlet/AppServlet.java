dc7ad3fbd94aa2fb425bbffca4efdcbae16520a3
StanZajdel <StanZajdel@StanZajdel.fios-router.home>
2017-07-12 19:32:09 -0400

    test

--- src/main/java/com/mydrawer/servlet/AppServlet.java
+++ src/main/java/com/mydrawer/servlet/AppServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.mydrawer.utility.FileUtility;
 
-@WebServlet(name = "mydrawer",urlPatterns = {"/mydrawer/*"})
+@WebServlet(name = "mydrawer",urlPatterns = {"/*"})
 public class AppServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 2857847752169838915L;
